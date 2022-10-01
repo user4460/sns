3532e69f62b75be11ca5abb9224e0144083c67aa
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-01 19:48:00 +0800

    firebase env

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,6 +1,6 @@
 /* ・ユーザー登録	 "・メールアドレス・パスワード（半角英数8文字以上）・Googleアカウント"	
 ・非ログインユーザー */
-
+/*
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth();
@@ -15,7 +15,7 @@ createUserWithEmailAndPassword(auth, email, password)
       const errorMessage = error.message;
       // ..
    });
-   
+*/
 const signup = () => {
    return (
       <div>
