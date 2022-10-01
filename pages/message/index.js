9cd32d549883de86afb3fa13dae07099d228adbc
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-01 13:04:45 +0800

    message room

---
+++ pages/message/index.js
@@ -0,0 +1,17 @@
+/*
+メッセージルーム
+https://firebase.google.com/docs/firestore/query-data/queries?hl=ja
+
+*/
+
+const mypage = () => {
+   return (
+      <div>
+         {/*"・フッターメニュー・ユーザー・"*/}
+         <h1>フッターメニュー</h1>
+         <h1>ユーザー・</h1>
+      </div>
+   );
+}
+
+export default mypage;
