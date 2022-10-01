3c1b43f392d9f760e2ed6ea543083bd71b2881f6
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-01 19:20:22 +0800

    firebase install

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,6 +1,21 @@
 /* ・ユーザー登録	 "・メールアドレス・パスワード（半角英数8文字以上）・Googleアカウント"	
 ・非ログインユーザー */
 
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+   .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+   })
+   .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+   });
+   
 const signup = () => {
    return (
       <div>
