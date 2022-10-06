0220d7da89cba9ad9eb320b42d656d15c6038927
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-06 15:56:07 +0800

    chakraui component

---
+++ pages/components/Listlink.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Link from "next/link";
+
+const Listlink = () => {
+   return (
+      <>
+   <Link href="/mypage">
+        <a>mypage</a>
+      </Link><br></br>
+      <Link href="/signin">
+        <a>singin</a>
+      </Link><br></br>
+      <Link href="/signup">
+        <a>signup</a>
+      </Link><br></br>
+      <Link href="/message">
+        <a>message</a>
+      </Link><br></br>
+      <Link href="/users">
+        <a>users</a>
+         </Link>
+         </>
+   )
+}
+
+export default Listlink;
