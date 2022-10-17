2eae2833e9f47389235ed295251312be18ac2ea3
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-17 12:14:37 +0800

    test

--- test/function/fcrud.test.js
+++ test/function/fcrud.test.js
@@ -1,2 +1,41 @@
 //cloud functions のテスト
 //firebase functions:config:get > .runtimeconfig.json 
+
+/*
+describe("todoを作成する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoを編集する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoを削除する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoをチェックする", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+*/
